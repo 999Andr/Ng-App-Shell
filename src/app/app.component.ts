1ed118d25c7da5209663fc25205265b4d0c469ea
999Andr <999ab9999999@gmail.com>
2020-10-01 00:15:23 +0400

    +code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'shell';
+  
+  message;
+  
+  constructor(@Inject(PLATFORM_ID) platformId: any) {
+    this.message = isPlatformBrowser(platformId) ?  
+    this.appMessage("Browser") : this.appMessage("App Shell")
+  }
+  
+  ngOnInit() { 
+    setTimeout(() => { return this.showTime(); }, 1000); 
+  }
+  
+  private appMessage(arg) { 
+    console.log(`renderer by ${arg}`); 
+    return `renderer by ${arg}`;
+  }
+  
+  showTime() { 
+    
+    if (this.message == "renderer by Browser") {   
+      return localStorage.getItem('First_Contentful_Paint');
+    }
+  
+  }
+
 }
+
